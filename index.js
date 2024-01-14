042714e21bc8199211fd03fd1c5e9ceeddaad8b4
Jazper <jazpersaldana@yahoo.com>
2024-01-14 14:01:25 -0600

    properly displaying materials images on webpage

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ function fetchData() {
     fetch('https://botw-compendium.herokuapp.com/api/v2')
     .then((resp) => resp.json())
     .then((data) => {
-        const treasureData = data['data']['materials']
-        console.log(treasureData)
+        const materialsData = data['data']['materials']
+        console.log(materialsData)
+
+        const imageContainer = document.getElementById('imageContainer')
+
+        materialsData.forEach((materials) => {
+            const imageUrl = materials['image']
+            // console.log(imageUrl)
+            const imageElement = document.createElement('img')
+
+            imageElement.src = imageUrl
+
+            imageContainer.appendChild(imageElement)
+        })
     })
 }

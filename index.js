de2b7d2a9c42aa1608ddea410a5155ecd9cfb088
Jazper <jazpersaldana@yahoo.com>
2024-01-14 10:45:38 -0600

    working fetch request

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => console.log('Files are connected'))
+document.addEventListener('DOMContentLoaded', () => fetchData())
+
+function fetchData() {
+    fetch('https://botw-compendium.herokuapp.com/api/v2')
+    .then((resp) => resp.json())
+    .then((data) => console.log(data))
+}

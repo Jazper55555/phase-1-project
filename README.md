bcb56085cd16236ff2a6c021fd84d3ffde00c7ce
Jazper <jazpersaldana@yahoo.com>
2024-01-14 15:12:38 -0600

    names are displaying over images when mouse hovers over them

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Users should be able to do the following when accessing the webpage:
     category
         Fetch data & display images from the treasure category
     FOREACH can be used here
-2. When you do a MOUSEOVER on any picture, it should display name
+2. When you do a MOUSEOVER on any picture, it should display name 
+    ID's can be assigned to each image allowing for easier filtering
 3. When you do a CLICK on any picture, it will display the description underneath image
--- index.js
+++ index.js
@@ -12,17 +12,34 @@ function fetchData() {
 
         materialsData.forEach((materials) => {
             const imageUrl = materials['image']
+            const name = materials['name']
+            // console.log(name)
             // console.log(imageUrl)
             const imageElement = document.createElement('img')
-
             imageElement.src = imageUrl
-
             imageElement.id = `${counter}`
             counter++
 
-            imageContainer.appendChild(imageElement)
-        })
-    })
-}
+            const tooltipElement = document.createElement('div')
+            tooltipElement.classList.add('tooltip')
+            tooltipElement.textContent = name
 
- 
+            imageElement.addEventListener('mouseover', (event) => {
+                const imageRect = event.target.getBoundingClientRect()
+                tooltipElement.style.top = `${imageRect.top}px`
+                tooltipElement.style.left = `${imageRect.left}px`
+                tooltipElement.classList.add('visible')
+                // const nameElement = document.getElementById('nameContainer')
+                // nameElement.textContent = name
+            })
+
+            imageElement.addEventListener('mouseout', () => {
+                tooltipElement.classList.remove('visible')
+            })
+
+            imageContainer.appendChild(imageElement)
+            imageContainer.appendChild(tooltipElement)
+        })
+
+    })
+} 
